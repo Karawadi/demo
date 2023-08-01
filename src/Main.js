eb64c78c47f313a3e1662ed79c3bfc3627bfb882
Karawadi <141113822+Karawadi@users.noreply.github.com>
2023-08-01 11:54:46 +0530

    upload a project

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+function Demo() {
+    return (
+        <div className="demo">
+            <h1>  hello students</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nesciunt inventore accusantium hic quisquam esse impedit consequatur repellendus! Unde voluptas accusantium quis reprehenderit et aliquam sit mollitia in placeat quia!
+            </p>
+        </div>
+    )
+}
+
+export default Demo
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Demo from './Main'
+import './style.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <Demo />
+  </div>
+);
+
+// 1. html 
+// 2. css 
+// 3. bootstap 
+
+// 4. js 
+// 5. json 
+// 6. react 
+
+// 7. nodejs 
+// 8. express 
+
+
+// .next.js
+// react native 
+// 9. mongodb 
+// 10. MySQL 
+
+// git/ github 
+
+// deploy
+
+
